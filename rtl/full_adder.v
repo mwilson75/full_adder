5fa6ea104e74df7a7a6f9fbb16b8c47e6a3b0ebc
Matthew Wilson <msw09g@gmail.com>
2025-10-28 18:02:02 -0400

    [refactor] Add ability to adjust bit width of adder terms

--- rtl/full_adder.v
+++ rtl/full_adder.v
@@ -1,21 +1,21 @@
-module full_adder (
-    input [3:0] a,
-    input [3:0] b,
+module full_adder #(parameter BIT_WIDTH = 4)
+(
+    input [BIT_WIDTH-1:0] a,
+    input [BIT_WIDTH-1:0] b,
     input cin,
-    output [3:0] sum,
+    output [BIT_WIDTH-1:0] sum,
     output cout
 );
-parameter BIT_DEPTH = 4;
 
 genvar i;
-wire[BIT_DEPTH:0] tmpcout;
+wire[BIT_WIDTH:0] tmpcout;
 assign tmpcout[0] = cin;
 generate 
-    for(i=0; i < BIT_DEPTH;i++) begin 
+    for(i=0; i < BIT_WIDTH;i++) begin 
         one_bit_adder inst (.a(a[i]),.b(b[i]),.cin(tmpcout[i]),.sum(sum[i]),.cout(tmpcout[i+1]));
     end 
 endgenerate
 
-assign cout = tmpcout[BIT_DEPTH];
+assign cout = tmpcout[BIT_WIDTH];
 
 endmodule
--- tb/full_adder_TB.v
+++ tb/full_adder_TB.v
@@ -4,7 +4,7 @@ parameter BIT_WIDTH = 4;
 reg [BIT_WIDTH-1:0] a = '0,b = '0,sum,verilog_sum;
 reg cin = '0,cout,verilog_cout;
 
-full_adder UUT
+full_adder #(.BIT_WIDTH(BIT_WIDTH)) UUT
 (
     .a(a),
     .b(b),
@@ -19,10 +19,11 @@ initial begin
     assert(sum == verilog_sum);
     assert(cout == verilog_cout);
     #2;
-    a = 4'hf;
-    b = 4'hf;
+    a = 15;
+    b = 15;
     cin = 1;
     {verilog_cout,verilog_sum} = a + b + cin;
+    #1;
     assert(sum == verilog_sum);
     assert(cout == verilog_cout);
 
